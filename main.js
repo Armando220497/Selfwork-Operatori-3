2a0f7ca0fba956881f16f276a71deff0b57d1b87
Armando Di Santo <armando.di.santo@hotmail.com>
2024-05-09 01:08:59 +0200

    inserito prompt per inserimento numeri

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let num1 = 10;
-let num2 = 5;
+let num1 = parseInt(prompt("Inserisci il primo numero"));
+let num2 = parseInt(prompt("Inserisci il secondo numero"));
 
 // somma
 let somma = num1 + num2;
@@ -17,4 +17,5 @@ let divisione = num1 / num2;
 let potenza = num1 ** num2;
 
 
-console.log("Con i numeri da te scelti, i risultati delle varie operazioni sono: Somma=" + somma + " Sottrazione=" + sottrazione + " Moltiplicazione=" + moltiplicazione + " Divisione=" + divisione + " Potenza=" + potenza);
+console.log("Con i numeri da te scelti, i risultati delle varie operazioni sono: ");
+console.log("Somma=" + somma + " Sottrazione=" + sottrazione + " Moltiplicazione=" + moltiplicazione + " Divisione=" + divisione + " Potenza=" + potenza);
