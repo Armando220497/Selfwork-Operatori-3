7d6a42f41b768c6dd571c7a7c9a4fcbf90183f54
Armando Di Santo <armando.di.santo@hotmail.com>
2024-05-09 01:00:59 +0200

    Selfwork-operazioni-3

---
+++ main.js
@@ -0,0 +1,20 @@
+let num1 = 10;
+let num2 = 5;
+
+// somma
+let somma = num1 + num2;
+
+// sottrazione
+let sottrazione = num1 - num2;
+
+// moltiplicazione
+let moltiplicazione = num1 * num2;
+
+// divisione
+let divisione = num1 / num2;
+
+// potenza con esponente 5 e base 10
+let potenza = num1 ** num2;
+
+
+console.log("Con i numeri da te scelti, i risultati delle varie operazioni sono: Somma=" + somma + " Sottrazione=" + sottrazione + " Moltiplicazione=" + moltiplicazione + " Divisione=" + divisione + " Potenza=" + potenza);
